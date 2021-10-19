8caeb2780089f59a5696ae1c9ffeb43a885de291
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-19 15:47:05 +0530

    Debugging

---
+++ dxc/src/Debuging/Calculator.java
@@ -0,0 +1,7 @@
+package Debuging;
+
+public class Calculator {
+		int add(int num1,int num2) {
+			return num1+num2;
+		}
+}
---
+++ dxc/src/Debuging/Debug.java
@@ -0,0 +1,13 @@
+package Debuging;
+
+public class Debug {
+
+	public static void main(String[] args) {
+		Calculator c1=new Calculator();
+		int sum=c1.add(11, 22);
+		System.out.println("the sum is-"+sum);
+		throw new NullPointerException();
+
+	}
+
+}
