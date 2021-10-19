e8753130ac2baed12b7568616c8cd0d7d921433c
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-19 12:44:40 +0530

    Control statement

---
+++ dxc/src/ControlStatements/IfelseStatement.java
@@ -0,0 +1,15 @@
+package ControlStatements;
+
+public class IfelseStatement {
+
+	public static void main(String[] args) {
+		int age=20;
+		if(age>18) {
+			System.out.println("age is greater then 18");
+		}else {
+			System.out.println("age is less then 18");
+		}
+		
+	}
+
+}
