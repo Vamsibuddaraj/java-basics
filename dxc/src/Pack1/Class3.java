16d493d45e65a0e125345182009ab525bb6c6cb1
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-21 11:24:56 +0530

    Commit

---
+++ dxc/src/Pack1/Class3.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class1 cla1=new Class1();
+		Class1 cla2=new Class2();
+		System.out.println(cla2.Tax(9999));
+	}
+
+}
